c12d990275a6a5b22c6d09c58a6d3ae6e203e62c
Claudia <claudia-pineda1@outlook.com>
2022-11-07 13:35:50 -0500

    my first commit

---
+++ proyecto_back/routes/empleados.router.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const empleadosController = require("../controllers/empleados.controller");
+
+router.post("/", empleadosController.create)
+
+module.exports = router
